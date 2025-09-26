d366205fb1c2c1e98d44104011f583c673601f44
Yazid Talbi <yazidtalbi@MacBook-Pro-de-Yazid.local>
2025-09-26 11:54:35 +0100

    vercel test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/serverless"; // or '@astrojs/vercel/edge'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // or 'hybrid',
+  output: "server", // IMPORTANT: SSR build
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
